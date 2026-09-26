3a4bdf26946cc8defd56e7003cf67f11e5cce65a
Kan_Jun_Hong <junhongkannot@gmail.com>
2026-09-26 08:28:56 +0800

    ui: Remove unused import

--- src/main/java/libconnect/ui/components/Navbar.java
+++ src/main/java/libconnect/ui/components/Navbar.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import libconnect.models.AccountType;
 import libconnect.models.Librarian;
 import libconnect.models.User;
 import libconnect.ui.SceneNavigator;
